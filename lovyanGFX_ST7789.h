5ea056e565291028281fa3e0a08cdc9fe60cb0d0
dazzor <dazzor_2000@yahoo.com>
2026-01-20 13:12:44 -0600

    translated - lovyanGFX_ST7789.h
    translated most of the Japanese to English

--- lovyanGFX_ST7789.h
+++ lovyanGFX_ST7789.h
@@ -1,39 +1,29 @@
 #pragma once
-
 #define LGFX_USE_V1
-
 #include <LovyanGFX.hpp>
 
-// ESP32でLovyanGFXを独自設定で利用する場合の設定例
+// Example of configuration for using LovyanGFX with custom settings on ESP32.
+/* Please duplicate this file, give it a new name, and modify the settings to suit your environment.
+The created files can be used by including them in the user program.。
+You can place the copied files in the lgfx_user folder of the library and use them, 
+but please note that they may be deleted when the library is updated.
+If you want to operate it safely, please create a backup or place it in your user project folder.
+//*/
+/// Create a class that performs custom settings by deriving it from LGFX_Device.
+class LGFX : public lgfx::LGFX_Device {
+/*The class name can be changed from "LGFX" to a different name.
+When using it with AUTODETECT, "LGFX" is already in use, so please change it to a name other than LGFX.
+Also, if you are using multiple panels simultaneously, please give each one a different name.
+ ※ If you change the class name, you must also change the constructor name to match the new class name.
 
-/*
-このファイルを複製し、新しい名前を付けて、環境に合わせて設定内容を変更してください。
-作成したファイルをユーザープログラムからincludeすることで利用可能になります。
-
-複製したファイルはライブラリのlgfx_userフォルダに置いて利用しても構いませんが、
-その場合はライブラリのアップデート時に削除される可能性があるのでご注意ください。
-
-安全に運用したい場合はバックアップを作成しておくか、ユーザープロジェクトのフォルダに置いてください。
+You can choose the name freely, but considering the possibility of increased settings in the future,
+for example, if you configure an ILI9341 with SPI connection on an ESP32 DevKit-C,
+you could name it: LGFX_DevKitC_SPI_ILI9341
+By using names like this and ensuring that the file name and class name match, 
+it becomes easier to use the code without confusion.
 //*/
 
-
-/// 独自の設定を行うクラスを、LGFX_Deviceから派生して作成します。
-class LGFX : public lgfx::LGFX_Device
-{
-/*
- クラス名は"LGFX"から別の名前に変更しても構いません。
- AUTODETECTと併用する場合は"LGFX"は使用されているため、LGFX以外の名前に変更してください。
- また、複数枚のパネルを同時使用する場合もそれぞれに異なる名前を付けてください。
- ※ クラス名を変更する場合はコンストラクタの名前も併せて同じ名前に変更が必要です。
-
- 名前の付け方は自由に決めて構いませんが、設定が増えた場合を想定し、
- 例えばESP32 DevKit-CでSPI接続のILI9341の設定を行った場合、
-  LGFX_DevKitC_SPI_ILI9341
- のような名前にし、ファイル名とクラス名を一致させておくことで、利用時に迷いにくくなります。
-//*/
-
-
-// 接続するパネルの型にあったインスタンスを用意します。
+// Prepare an instance that matches the type of panel you want to connect.
 //lgfx::Panel_GC9A01      _panel_instance;
 //lgfx::Panel_GDEW0154M09 _panel_instance;
 //lgfx::Panel_HX8357B     _panel_instance;
@@ -58,15 +48,15 @@ lgfx::Panel_ST7789      _panel_instance;
 //lgfx::Panel_ST7796      _panel_instance;
 
 
-// パネルを接続するバスの種類にあったインスタンスを用意します。
-  lgfx::Bus_SPI       _bus_instance;   // SPIバスのインスタンス
-//lgfx::Bus_I2C       _bus_instance;   // I2Cバスのインスタンス (ESP32のみ)
-//lgfx::Bus_Parallel8 _bus_instance;   // 8ビットパラレルバスのインスタンス (ESP32のみ)
+// Prepare an instance that matches the type of bus used to connect the panel.
+  lgfx::Bus_SPI       _bus_instance;   // SPI bus instance
+//lgfx::Bus_I2C       _bus_instance;   // I2C bus instance (ESP32 only)
+//lgfx::Bus_Parallel8 _bus_instance;   // 8-bit parallel bus instance (ESP32 only)
 
-// バックライト制御が可能な場合はインスタンスを用意します。(必要なければ削除)
+// If backlight control is possible, an instance will be created. (Delete it if not needed.)
   lgfx::Light_PWM     _light_instance;
 
-// タッチスクリーンの型にあったインスタンスを用意します。(必要なければ削除)
+// Prepare an instance that matches the type of touchscreen. (Delete if not needed.)
 //lgfx::Touch_CST816S          _touch_instance;
 //  lgfx::Touch_FT5x06           _touch_instance; // FT5206, FT5306, FT5406, FT6206, FT6236, FT6336, FT6436
 //lgfx::Touch_GSL1680E_800x480 _touch_instance; // GSL_1680E, 1688E, 2681B, 2682B
@@ -80,31 +70,31 @@ lgfx::Touch_XPT2046          _touch_instance;
 
 public:
 
-  // コンストラクタを作成し、ここで各種設定を行います。
-  // クラス名を変更した場合はコンストラクタも同じ名前を指定してください。
+  // We create a constructor and perform various settings here.
+  // If you change the class name, please also specify the same name for the constructor.
   LGFX(void)
   {
-    { // バス制御の設定を行います。
-      auto cfg = _bus_instance.config();    // バス設定用の構造体を取得します。
+    { // We will configure the bus control settings.
+      auto cfg = _bus_instance.config();    // Retrieves the structure used for bus configuration.
 
-// SPIバスの設定
-      cfg.spi_host = VSPI_HOST;     // 使用するSPIを選択  ESP32-S2,C3 : SPI2_HOST or SPI3_HOST / ESP32 : VSPI_HOST or HSPI_HOST
-      // ※ ESP-IDFバージョンアップに伴い、VSPI_HOST , HSPI_HOSTの記述は非推奨になるため、エラーが出る場合は代わりにSPI2_HOST , SPI3_HOSTを使用してください。
-      cfg.spi_mode = 0;             // SPI通信モードを設定 (0 ~ 3)
-      cfg.freq_write = 40000000;    // 送信時のSPIクロック (最大80MHz, 80MHzを整数で割った値に丸められます)
-      cfg.freq_read  = 16000000;    // 受信時のSPIクロック
-      cfg.spi_3wire  = true;        // 受信をMOSIピンで行う場合はtrueを設定
-      cfg.use_lock   = true;        // トランザクションロックを使用する場合はtrueを設定
-      cfg.dma_channel = SPI_DMA_CH_AUTO; // 使用するDMAチャンネルを設定 (0=DMA不使用 / 1=1ch / 2=ch / SPI_DMA_CH_AUTO=自動設定)
-      // ※ ESP-IDFバージョンアップに伴い、DMAチャンネルはSPI_DMA_CH_AUTO(自動設定)が推奨になりました。1ch,2chの指定は非推奨になります。
-      cfg.pin_sclk = 14;            // SPIのSCLKピン番号を設定
-      cfg.pin_mosi = 13;            // SPIのMOSIピン番号を設定
-      cfg.pin_miso = 12;            // SPIのMISOピン番号を設定 (-1 = disable)
-      cfg.pin_dc   = 2;            // SPIのD/Cピン番号を設定  (-1 = disable)
-     // SDカードと共通のSPIバスを使う場合、MISOは省略せず必ず設定してください。
+// SPI bus settings
+      cfg.spi_host = SPI3_HOST; //VSPI_HOST;     // Select the SPI to use.  ESP32-S2,C3 : SPI2_HOST or SPI3_HOST / ESP32 : VSPI_HOST or HSPI_HOST
+      // ※ Due to the ESP-IDF version update, the use of VSPI_HOST and HSPI_HOST is now deprecated. If you encounter errors, please use SPI2_HOST and SPI3_HOST instead.
+      cfg.spi_mode = 0;             // Set the SPI communication mode (0 to 3).
+      cfg.freq_write = 40000000;    // SPI clock frequency during transmission (maximum 80MHz, rounded to a value obtained by dividing 80MHz by an integer)
+      cfg.freq_read  = 16000000;    // SPI clock during reception
+      cfg.spi_3wire  = true;        // Set to true if receiving data via the MOSI pin.
+      cfg.use_lock   = true;        // Set to true if you want to use transaction locks.
+      cfg.dma_channel = SPI_DMA_CH_AUTO; // Set the DMA channel to be used (0=No DMA / 1=Channel 1 / 2=Channel 2 / SPI_DMA_CH_AUTO=Automatic setting)
+      // ※ With the ESP-IDF version update, using SPI_DMA_CH_AUTO (automatic setting) for the DMA channel is now recommended. Specifying channels 1 or 2 is no longer recommended.
+      cfg.pin_sclk = 14;            // Set the SPI SCLK pin number.
+      cfg.pin_mosi = 13;            // Set the SPI MOSI pin number.
+      cfg.pin_miso = 12;            // Set the SPI MISO pin number (-1 = disable)
+      cfg.pin_dc   = 2;            // Set the SPI D/C pin number (-1 = disable)
+     // When using the SPI bus, which is shared with the SD card, be sure to configure the MISO pin; do not omit it.
 //*/
 /*
-// I2Cバスの設定
+// I2C bus settings
       cfg.i2c_port    = 0;          // 使用するI2Cポートを選択 (0 or 1)
       cfg.freq_write  = 400000;     // 送信時のクロック
       cfg.freq_read   = 400000;     // 受信時のクロック
@@ -113,7 +103,7 @@ public:
       cfg.i2c_addr    = 0x3C;       // I2Cデバイスのアドレス
 //*/
 /*
-// 8ビットパラレルバスの設定
+// 8-bit parallel bus settings
       cfg.i2s_port = I2S_NUM_0;     // 使用するI2Sポートを選択 (I2S_NUM_0 or I2S_NUM_1) (ESP32のI2S LCDモードを使用します)
       cfg.freq_write = 20000000;    // 送信クロック (最大20MHz, 80MHzを整数で割った値に丸められます)
       cfg.pin_wr =  4;              // WR を接続しているピン番号
@@ -129,46 +119,46 @@ public:
       cfg.pin_d7 = 14;              // D7を接続しているピン番号
 //*/
 
-      _bus_instance.config(cfg);    // 設定値をバスに反映します。
-      _panel_instance.setBus(&_bus_instance);      // バスをパネルにセットします。
+      _bus_instance.config(cfg);    // The settings will be applied to the bus.
+      _panel_instance.setBus(&_bus_instance);      // Set the bus onto the panel.
     }
 
-    { // 表示パネル制御の設定を行います。
-      auto cfg = _panel_instance.config();    // 表示パネル設定用の構造体を取得します。
+    { // Configure the display panel settings.
+      auto cfg = _panel_instance.config();    // Retrieves the structure used for display panel settings.
 
-      cfg.pin_cs           =    15;  // CSが接続されているピン番号   (-1 = disable)
-      cfg.pin_rst          =    -1;  // RSTが接続されているピン番号  (-1 = disable)
-      cfg.pin_busy         =    -1;  // BUSYが接続されているピン番号 (-1 = disable)
+      cfg.pin_cs           =    15;  // The pin number to which CS is connected.   (-1 = disable)
+      cfg.pin_rst          =    -1;  // The pin number to which RST is connected.  (-1 = disable)
+      cfg.pin_busy         =    -1;  // The pin number to which BUSY is connected. (-1 = disable)
 
-      // ※ 以下の設定値はパネル毎に一般的な初期値が設定されていますので、不明な項目はコメントアウトして試してみてください。
+      // ※ The following settings have general default values ​​set for each panel, so if you are unsure about any item, try commenting it out.
 
-      cfg.panel_width      =   240;  // 実際に表示可能な幅
-      cfg.panel_height     =   320;  // 実際に表示可能な高さ
-      cfg.offset_x         =     0;  // パネルのX方向オフセット量
-      cfg.offset_y         =     0;  // パネルのY方向オフセット量
+      cfg.panel_width      =   240;  // The actual displayable width
+      cfg.panel_height     =   320;  // The actual displayable height
+      cfg.offset_x         =     0;  // Panel X-direction offset amount
+      cfg.offset_y         =     0;  // Panel Y-direction offset amount
       cfg.offset_rotation  =     0;  // Rotation direction value offset: 0-7 (4-7 are vertically inverted)
       cfg.dummy_read_pixel =     8;  // Number of dummy read bits before pixel readout
-      cfg.dummy_read_bits  =     1;  // ピクセル以外のデータ読出し前のダミーリードのビット数
-      cfg.readable         =  true;  // データ読出しが可能な場合 trueに設定
-      cfg.invert           = false;  // パネルの明暗が反転してしまう場合 trueに設定
-      cfg.rgb_order        = false;  // パネルの赤と青が入れ替わってしまう場合 trueに設定
-      cfg.dlen_16bit       = false;  // 16bitパラレルやSPIでデータ長を16bit単位で送信するパネルの場合 trueに設定
-      cfg.bus_shared       =  true;  // SDカードとバスを共有している場合 trueに設定(drawJpgFile等でバス制御を行います)
+      cfg.dummy_read_bits  =     1;  // The number of dummy read bits before reading data other than pixels.
+      cfg.readable         =  true;  // Set to true if data can be read.
+      cfg.invert           = false;  // Set to true if the panel's brightness and darkness are reversed.
+      cfg.rgb_order        = false;  // Set to true if the red and blue colors on the panel are reversed.
+      cfg.dlen_16bit       = false;  // Set this to true for panels that transmit data in 16-bit units via 16-bit parallel or SPI interfaces.
+      cfg.bus_shared       =  true;  // Set this to true if the SD card shares the bus (bus control will be performed by functions such as drawJpgFile).
 
-// 以下はST7735やILI9163のようにピクセル数が可変のドライバで表示がずれる場合にのみ設定してください。
-//    cfg.memory_width     =   240;  // ドライバICがサポートしている最大の幅
-//    cfg.memory_height    =   320;  // ドライバICがサポートしている最大の高さ
+// The following settings should only be configured if the display is misaligned with drivers that have a variable number of pixels, such as the ST7735 or ILI9163.
+//    cfg.memory_width     =   240;  // The maximum width supported by the driver IC.
+//    cfg.memory_height    =   320;  // The maximum height supported by the driver IC.
 
       _panel_instance.config(cfg);
     }
 
 //*
-    { // バックライト制御の設定を行います。（必要なければ削除）
-      auto cfg = _light_instance.config();    // バックライト設定用の構造体を取得します。
+    { // Configure the backlight control settings. (Delete if not needed)
+      auto cfg = _light_instance.config();    // Retrieves the structure for backlight settings.
 
-      cfg.pin_bl = 21;              // バックライトが接続されているピン番号
-      cfg.invert = false;           // バックライトの輝度を反転させる場合 true
-      cfg.freq   = 44100;           // バックライトのPWM周波数
+      cfg.pin_bl = 21;              // The pin number to which the backlight is connected.
+      cfg.invert = false;           // Set to true to invert the backlight brightness.
+      cfg.freq   = 44100;           // Backlight PWM frequency
       cfg.pwm_channel = 7;          // The channel number of the PWM to be used.
 
       _light_instance.config(cfg);
@@ -181,22 +171,22 @@ public:
       auto cfg = _touch_instance.config();
 
       cfg.x_min      = 0;    // The minimum X value (raw value) obtained from the touchscreen.
-      cfg.x_max      = 239;  // タッチスクリーンから得られる最大のX値(生の値)
-      cfg.y_min      = 0;    // タッチスクリーンから得られる最小のY値(生の値)
-      cfg.y_max      = 319;  // タッチスクリーンから得られる最大のY値(生の値)
-      cfg.pin_int    = 36;   // The pin number to which INT is connected.
+      cfg.x_max      = 239;  // The maximum X value (raw value) obtained from the touchscreen.
+      cfg.y_min      = 0;    // The minimum Y-value (raw value) obtained from the touchscreen.
+      cfg.y_max      = 319;  // The maximum Y-value (raw value) obtained from the touchscreen.
+      cfg.pin_int    = 36;   // The pin number to which INT is connected.(IRQ)
       cfg.bus_shared = true; // Set to true if the same bus is used for both the screen and the device.
       cfg.offset_rotation = 0;// Adjustment for cases where the display and touch orientation do not match. Set a value between 0 and 7.
 
-// SPI接続の場合
+// For SPI connection
       cfg.spi_host = HSPI_HOST;// Select the SPI to use. (HSPI_HOST or VSPI_HOST)
       cfg.freq = 1000000;     // Set the SPI clock.
       cfg.pin_sclk = 25;     // The pin number to which SCLK is connected.
-      cfg.pin_mosi = 32;     // MOSIが接続されているピン番号
-      cfg.pin_miso = 39;     // MISOが接続されているピン番号
-      cfg.pin_cs   =  33;     //   CSが接続されているピン番号
+      cfg.pin_mosi = 32;     // The pin number to which MOSI is connected
+      cfg.pin_miso = 39;     // The pin number to which MISO is connected
+      cfg.pin_cs   =  33;     //   The pin number to which CS is connected.
 
-// I2C接続の場合
+// For I2C connection
 //      cfg.i2c_port = 1;      // 使用するI2Cを選択 (0 or 1)
 //      cfg.i2c_addr = 0x38;   // I2C device address number
 //      cfg.pin_sda  = 25;     // SDAが接続されているピン番号
@@ -204,11 +194,11 @@ public:
 //      cfg.freq = 400000;     // I2Cクロックを設定
 
       _touch_instance.config(cfg);
-      _panel_instance.setTouch(&_touch_instance);  // タッチスクリーンをパネルにセットします。
+      _panel_instance.setTouch(&_touch_instance);  // Install the touchscreen onto the panel.。
     }
 //*/
 
-    setPanel(&_panel_instance); // 使用するパネルをセットします。
+    setPanel(&_panel_instance); // Set the panels to be used.
   }
 };
 
